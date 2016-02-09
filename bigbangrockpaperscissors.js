5bf7d70c8373a73df1c80cf7176d8678d5ecc335
Ceronza <adam.james.mason@gmail.com>
2016-02-09 01:26:22 +0000

    more changes, im lazy

--- bigbangrockpaperscissors.js
+++ bigbangrockpaperscissors.js
@@ -1,11 +1,16 @@
+/*Starts the initial user input */
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 
+
+/* A loop check ensure correct input is given */
 while (userChoice != "rock" && userChoice != "scissors" && userChoice != "paper") {
     userChoice = prompt("Wrong selection. Please select from rock, paper or scissors.");
 }
 
-var computerChoice = Math.random();
 
+
+/*These calculate the random computer's input */
+var computerChoice = Math.random();
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
@@ -14,14 +19,19 @@ if (computerChoice < 0.34) {
 	computerChoice = "scissors";
 } 
 
-console.log("The User has chosen: " + userChoice);
-console.log("The computer has chosen: " + computerChoice);
+/*console.log("The User has chosen: " + userChoice);
+console.log("The computer has chosen: " + computerChoice);*/
 
+
+/* a loop to restart if there is a tie. still working on this one */
+while (userChoice === computerChoice) {
+    userChoice = prompt("The result is a tie. Let's try again. Select rock scissors or paper.");
+}
+
+
+/* the compare function that defines which input wins */
 var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    }
-    else if (choice1 === "rock") {
+    if (choice1 === "rock") {
         if (choice2 === "scissors") {
             return "rock wins";
         }
@@ -47,4 +57,7 @@ var compare = function(choice1, choice2) {
     }
 }
 
+
+
+/* This calls the function and prints the result to the browser */
 document.write(compare(userChoice, computerChoice));
